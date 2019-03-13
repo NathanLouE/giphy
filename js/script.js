34a49f27f0bc059d36c2639e8239ad2975232bbe
Nathan Louie <nathanlou22@gmail.com>
2019-03-13 00:55:56 +0000

    random

--- js/script.js
+++ js/script.js
@@ -18,3 +18,19 @@ $("#search-button").click(function(){
   
   
 });
+
+$("#random").click(function(){
+  var search = $("#search-term").val();
+  var random = Math.random() * 100;
+  var request_url = `https://api.giphy.com/v1/gifs/search?q=${search}&rating=pg&api_key=dc6zaTOxFJmzC`
+  $.ajax({
+      url: request_url,
+      method: "GET",
+      success: function(response){
+          $("#image").append(`<img src=${response.data[Math.floor(random)].images.original.url} >`);
+          //console.log(response.data[].images.original.url);
+      }
+  })
+  
+  
+});
